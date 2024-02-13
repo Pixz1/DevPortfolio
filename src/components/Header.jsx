8172cb28b2c65984f288d10953d982b7565d7fa6
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-02-13 23:00:38 +1000

    Add responsive design to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,40 @@
 import './Header.css'
+import React, { useState, useEffect } from 'react'
 
 export default function Header() {
+
+	// open/close menu
+	const [menuOpen, setMenuOpen] = useState(false)
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen)
+	}
+
+	// useEffect to toggles scrollbar based on menuOpen state
+	const toggleBodyScroll = (disableScroll) => {
+		if (disableScroll) document.body.style.overflow = 'hidden'
+		else document.body.style.overflow = 'auto'
+	}
+	useEffect(() => {
+		toggleBodyScroll(menuOpen)
+	}, [menuOpen])
+
 	return (
 		<div className='navbar'>
 			<nav>
 				<a href='#'>
 					<h2>davo.dev</h2>
 				</a>
-				<ul className='nav-links'>
-					<li><a href='#home' className='nav-link'>Home</a></li>
-					<li><a href='#about' className='nav-link'>About</a></li>
-					<li><a href='#projects' className='nav-link'>Projects</a></li>
-					<li><a href='#contact' className='nav-link'>Contact</a></li>
+				<ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+					<li><a href='#home' className='nav-link' onClick={(toggleMenu)}>Home</a></li>
+					<li><a href='#about' className='nav-link' onClick={(toggleMenu)}>About</a></li>
+					<li><a href='#projects' className='nav-link' onClick={(toggleMenu)}>Projects</a></li>
+					<li><a href='#contact' className='nav-link' onClick={(toggleMenu)}>Contact</a></li>
 				</ul>
+				<div className={`hamburger ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+					<span className='bar'></span>
+					<span className='bar'></span>
+					<span className='bar'></span>
+				</div>
 			</nav>
 		</div>
 	)
