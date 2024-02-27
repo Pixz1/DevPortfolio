70e01245d8325240de4063d784be6b354a927b12
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-02-27 17:13:40 +1000

    Update icons in Projects component to be svg instead of png

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,6 @@
 import './Projects.css'
-import { useState, useEffect } from 'react'
 
 export default function Projects() {
-
-    // swaps img src based on colour mode
-    const [codeSrc, setCodeSrc] = useState('/code.png')
-    const [openNewSrc, setOpenNewSrc] = useState('/open-new.png')
-    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)')
-    darkModeQuery.addEventListener('change', (event) => {
-        if (event.matches) {
-            setCodeSrc('/code-white.png')
-            setOpenNewSrc('/open-new-white.png')
-        }
-        else {
-            setCodeSrc('/code.png')
-            setOpenNewSrc('/open-new.png')
-        }
-    })
-    useEffect(() => {
-        if (darkModeQuery) {
-            setCodeSrc('/code-white.png')
-            setOpenNewSrc('/open-new-white.png')
-        }
-    }, [])
-
     return (
         <section id='projects'>
             <div className='projects-container'>
@@ -53,22 +30,18 @@ export default function Projects() {
                                         href='https://github.com/Pixz1/quiz-app'
                                         target='_blank'
                                         rel='noopener noreferrer'
-                                        id='code'
                                     >
-                                        <img 
-                                            src={codeSrc}
-                                            alt='Code Link' 
-                                            className='code-icon'
-                                            height='30px'
-                                            width='30px'
-                                        />
-                                        <img 
-                                            src='/code-hover.png' 
-                                            alt='Code Link' 
-                                            className='code-hover-icon'
-                                            height='30px'
-                                            width='30px'
-                                        />
+                                        <svg 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            width="25" 
+                                            height="25" 
+                                            viewBox="0 0 24 24"
+                                            className='github-svg'
+                                        >
+                                            <path 
+                                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                                            />
+                                        </svg>
                                     </a>
                                 </div>
                                 <div className='live-link'>
@@ -77,22 +50,22 @@ export default function Projects() {
                                         href='#'
                                         target='_blank'
                                         rel='noopener noreferrer'
-                                        id='open-new'
                                     >
-                                        <img 
-                                            src={openNewSrc} 
-                                            alt='Open in new window' 
-                                            className='open-new-icon'
-                                            height='30px'
-                                            width='30px'
-                                        />
-                                        <img 
-                                            src='/open-new-hover.png' 
-                                            alt='Open in new window' 
-                                            className='open-new-hover-icon'
-                                            height='30px'
-                                            width='30px'
-                                        />
+                                        <svg 
+                                            width="35px" 
+                                            height="35px" 
+                                            viewBox="0 0 24 24" 
+                                            fill="none" 
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className='new-window-svg'
+                                        >
+                                            <path 
+                                                d="M5 12V6C5 5.44772 5.44772 5 6 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H12M8.11111 12H12M12 12V15.8889M12 12L5 19" 
+                                                stroke="#464455" 
+                                                stroke-linecap="round" 
+                                                stroke-linejoin="round"
+                                            />
+                                        </svg>
                                     </a>
                                     <span className='tooltip-text'>
                                         Link currently not available
