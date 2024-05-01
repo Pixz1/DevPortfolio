bf2c41da079aa9529f614ce321ab31cad925138d
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-05-01 20:47:08 +1000

    Refactor tech icons to use constants array and rendered with map

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,34 @@
-import './About.css'
+import { techStack } from "../constants";
+import "./About.css";
 
 export default function About() {
     return (
-        <section id='about'>
-            <div className='about-container'>
-                <div className='about-heading'> 
+        <section id="about">
+            <div className="about-container">
+                <div className="about-heading">
                     <h1>About Me</h1>
                 </div>
-                <div className='about-info'>
+                <div className="about-info">
                     <p>
-                        I am an aspiring software developer, currently focusing on mastering front-end 
-                        development. I'm eager to apply my skills in a collaborative environment where I can
-                        learn, grow, and contribute to meaningful projects. If you're looking for a motivated 
-                        and adaptable developer, please reach out!
+                        I am an aspiring software developer, currently focusing
+                        on mastering front-end development. I'm eager to apply
+                        my skills in a collaborative environment where I can
+                        learn, grow, and contribute to meaningful projects. If
+                        you're looking for a motivated and adaptable developer,
+                        please reach out!
                     </p>
                 </div>
-                <div className='about-tech'>
+                <div className="about-tech">
                     <p>Current Tech Stack</p>
-                    <ul className='tech-icons'>
-                        <li className='tech-icon'>
-                            <img src='https://skillicons.dev/icons?i=html' alt='Html'/>
-                        </li>
-                        <li className='tech-icon'>
-                            <img src='https://skillicons.dev/icons?i=css' alt='Css'/>
+                    <ul className="tech-icons">
+                        {techStack.map((tech, index) => (
+                            <li key={index} className="tech-icon">
+                                <img src={tech.iconUrl} alt={tech.altText} />
                             </li>
-                        <li className='tech-icon'>
-                            <img src='https://skillicons.dev/icons?i=js' alt='JavaScript'/>
-                        </li>
-                        <li className='tech-icon'>
-                            <img src='https://skillicons.dev/icons?i=react' alt='React'/>
-                        </li>
+                        ))}
                     </ul>
-                </div> 
+                </div>
             </div>
         </section>
-    )
+    );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const techStack = [
+    {
+        iconUrl: "https://skillicons.dev/icons?i=html",
+        altText: "Html",
+    },
+    {
+        iconUrl: "https://skillicons.dev/icons?i=css",
+        altText: "Css",
+    },
+    {
+        iconUrl: "https://skillicons.dev/icons?i=js",
+        altText: "JavaScript",
+    },
+    {
+        iconUrl: "https://skillicons.dev/icons?i=react",
+        altText: "React",
+    },
+];
