a41bf1ec9183a8baddbb7ee69b99cf99f26e324b
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-05-22 23:31:31 +1000

    add new projects to project list

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,23 +56,37 @@ export const links = [
     },
 ];
 
-export const aboutInfo =
-    `I am an aspiring software developer, currently focusing on mastering front-end development. 
+export const aboutInfo = `I am an aspiring software developer, currently focusing on mastering front-end development. 
     I'm eager to apply my skills in a collaborative environment where I can learn, grow, 
     and contribute to meaningful projects. If you're looking for a motivated and 
     adaptable developer, please reach out!`;
 
 export const projects = [
     {
-        imgSrc: '/DevPortfolio/quiz-main.PNG',
-        title: 'Quizzy Time',
-        description: `Quizzy Time is a simple quiz web application, built with react 
-        using api from opentdb. Currently it has the following features: quiz progression, 
-        timer, hint, and navigation between questions.`,
-        link1: 'https://www.google.com/',
-        link2: 'https://www.google.com/'
+        imgSrc: "/DevPortfolio/threads_clone.jpg",
+        title: "Threads Clone",
+        description: `A simple clone of the Threads social media platform, built with Next JS, 
+        TypeScript, TailWind, Clerk as the auth service, and MongoDB as the database. Currently it allows logged in users to: view users, profiles
+        and activities, create a new thread or comment on existing threads, and join/create/view communities.`,
+        link1: "https://github.com/Pixz1/threads-clone",
+        link2: "https://threads-clone-weld-eta.vercel.app/",
     },
     {
-        title: 'Test'
-    }
-]
+        imgSrc: "/DevPortfolio/getaway_rentals.jpg",
+        title: "Getaway Rentals",
+        description: `Getaway Rentals is a car rental web app, built with Next JS,
+        TypeScript, and TailWind. Using API data from 'cars by ninja' and 'ImaginStudio' Currently it has no backend functionalities,
+        with the web app only providing a simple and detailed view of cars with their specifications.`,
+        link1: "https://github.com/Pixz1/car-rental",
+        link2: "https://getawayrentals-pixz1s-projects.vercel.app/",
+    },
+    {
+        imgSrc: "/DevPortfolio/quiz_main.PNG",
+        title: "Quizzy Time",
+        description: `Quizzy Time is a simple quiz web application, built with react 
+        using api from opentdb. Currently it has the following features: quiz progression, 
+        timer, hint, and navigation between questions. NOTE: live demo currently unavailable`,
+        link1: "https://github.com/Pixz1/quiz-app",
+        link2: "https://github.com/Pixz1/quiz-app",
+    },
+];
